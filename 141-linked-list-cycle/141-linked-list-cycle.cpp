bd9d364ab833ddb549a07e241dbb47de26ad9643
Zlozie Code <92716726+ZlozieCode@users.noreply.github.com>
2022-07-27 11:21:41 +0530

    Time: 41 ms (5.07%), Space: 11.3 MB (6.99%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        //O(N) Space
+        map<ListNode *,int>m;
+        ListNode *curr=head;
+        
+        while(curr!=NULL){
+            
+            if(!m[curr]){
+                m[curr]++;
+                curr=curr->next;
+                
+            }
+            
+            else{
+                return true;    
+            }
+        }
+        
+        return false;
+        
+    
+        //O(1) space
+//         ListNode *slow=head;
+//         ListNode *fast=head;
+        
+//         while(fast!=NULL && fast->next!=NULL){
+//             slow=slow->next;
+//             fast=fast->next->next;
+            
+//             if(slow==fast)return true;
+//         }
+        
+//         return false;
+    }
+};
