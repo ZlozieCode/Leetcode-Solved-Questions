f73eb64c34277f1013b7d35bdc3133239f3072e3
Zlozie Code <92716726+ZlozieCode@users.noreply.github.com>
2022-08-31 15:38:00 +0530

    Create Print Diagonally.cpp

---
+++ Print Diagonally.cpp
@@ -0,0 +1,27 @@
+// q - https://practice.geeksforgeeks.org/problems/print-diagonally4331/1
+
+class Solution{
+	
+	public:
+	vector<int> downwardDigonal(int n, vector<vector<int>> A)
+	{
+		// Your code goes here
+		int t=2*n-1;
+		
+		vector<vector<int>>v(t);
+		
+		for(int i=0;i<n;i++){
+		    for(int j=0;j<n;j++){
+		        v[i+j].push_back(A[i][j]);
+		    }
+		}
+		
+		vector<int>ans;
+		for(int i=0;i<v.size();i++){
+		    for(int j=0;j<v[0].size();j++){
+		        ans.push_back(v[i][j]);
+		    }
+		}
+		
+		return ans;
+	}
