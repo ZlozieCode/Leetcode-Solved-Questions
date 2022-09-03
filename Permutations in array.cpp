2c5520ea17d7b15d0c700f82938cf9f711de8cdf
Zlozie Code <92716726+ZlozieCode@users.noreply.github.com>
2022-09-03 21:56:11 +0530

    Create Permutations in array.cpp

---
+++ Permutations in array.cpp
@@ -0,0 +1,16 @@
+// q - https://practice.geeksforgeeks.org/problems/permutations-in-array1747/1
+
+class Solution {
+  public:
+    bool isPossible(long long a[], long long b[], int n, long long k) {
+        // Your code goes here
+        
+        sort(a,a+n);
+        sort(b,b+n);
+        for(int i=0;i<n;i++){
+            if(a[i]+b[n-1-i]<k)return false;
+        }
+        
+        return true;
+    }
+};
